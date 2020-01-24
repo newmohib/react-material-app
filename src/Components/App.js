4cfc9cb87b37b39c30e87f8ab6dcbe7ab6a558be
unknown <newmohib@gmail.com>
2020-01-24 20:00:02 +0600

    first commit

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from "react";
+import { Header, Footer } from "./Layouts";
+import Exercises from "./Exercises";
+
+export default class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Header />
+        <Exercises />n
+        <Footer />
+      </Fragment>
+    );
+  }
+}
